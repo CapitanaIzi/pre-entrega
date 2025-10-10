48144ff07ac0c4a6452489282629b536008ab0ae
Francesca <guizarfrancesca7@gmail.com>
2025-10-09 22:20:21 -0300

    get products

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+//console.log(process.argv);
+const [, , method, resource]=process.argv;
 
+if(method.toUpperCase() =="GET" && resource.toLowerCase()== "products"){
+fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())            
+            .then(json=>console.log(json))
+}

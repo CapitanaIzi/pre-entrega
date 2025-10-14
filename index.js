85c56d5e798c5b201e3c50b00dba00489f7bad7d
Francesca <guizarfrancesca7@gmail.com>
2025-10-14 20:15:29 -0300

    id en Get

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 //console.log(process.argv);
-const [, , method, resource]=process.argv;
+let [, , method, resource]=process.argv;
+method = method.toUpperCase();
+resource = resource.toLowerCase();
 
-if(method.toUpperCase() =="GET" && resource.toLowerCase()== "products"){
-fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())            
-            .then(json=>console.log(json))
+if (method == "GET" && resource.startsWith("products/")) {
+ 
+  let id = resource.split("/")[1];
+  id = parseInt(id);
+
+  if (isNaN(id) || id <= 0) {
+    console.log("No es un numero");
+   
+  }else {
+
+  fetch("https://fakestoreapi.com/products/" + id) 
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 }
